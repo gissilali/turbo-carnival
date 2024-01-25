3a5014ba16a06085aed6f7e5f6d85a6ef6b04340
Gibson Silali <gibsonsilali@gmail.com>
2024-01-25 23:09:58 +0300

    implemented simple routing

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -1,3 +1,49 @@
+import Dashboard from "./components/Dashboard.js";
+import Register from "./components/Register.js";
+
+export const navigateTo = (url) => {
+  history.pushState(null, null, url);
+  router();
+};
+
+const router = async () => {
+  const routes = [
+    {
+      path: "/",
+      component: Register,
+    },
+    {
+      path: "/dashboard",
+      component: Dashboard,
+    },
+  ];
+
+  const matchingPaths = routes.map((route) => ({
+    route,
+    isCurrent: route.path == location.pathname,
+  }));
+
+  let currentPath = matchingPaths.find((path) => path.isCurrent);
+
+  if (!currentPath) {
+    currentPath = {
+      route: routes.at(0),
+      isCurrent: true,
+    };
+  }
+
+  const component = new currentPath.route.component();
+
+  document.getElementById("app").innerHTML = await component.renderView();
+};
+
+const registerFormHandlers = async () => {
+  document
+    .getElementById("registerForm")
+    ?.addEventListener("submit", new Register().submitForm);
+};
+
 document.addEventListener("DOMContentLoaded", () => {
-  alert("hello, my friend.");
+  router();
+  registerFormHandlers();
 });
