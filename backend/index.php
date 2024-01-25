292fff988664f0abd235f4808e18787901b1c501
Gibson Silali <gibsonsilali@gmail.com>
2024-01-25 23:15:47 +0300

    Fixed CORS error

--- backend/index.php
+++ backend/index.php
@@ -1,5 +1,10 @@
 <?php
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+
+
 require __DIR__ . "/vendor/autoload.php";
 
 
