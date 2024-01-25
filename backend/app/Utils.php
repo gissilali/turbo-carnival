ef1e4bcff29278676c3412e68fd6d37bbd30214f
Gibson Silali <gibsonsilali@gmail.com>
2024-01-25 04:34:48 +0300

    Added Utils class, with helper methods

---
+++ backend/app/Utils.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App;
+
+class Utils
+{
+    public static function getUserIP(): string
+    {
+        if (getenv('HTTP_CLIENT_IP')) {
+            $ip = getenv('HTTP_CLIENT_IP');
+        } elseif (getenv('HTTP_X_FORWARDED_FOR')) {
+            $ip = getenv('HTTP_X_FORWARDED_FOR');
+        } elseif (getenv('HTTP_X_FORWARDED')) {
+            $ip = getenv('HTTP_X_FORWARDED');
+        } elseif (getenv('HTTP_FORWARDED_FOR')) {
+            $ip = getenv('HTTP_FORWARDED_FOR');
+        } elseif (getenv('HTTP_FORWARDED')) {
+            $ip = getenv('HTTP_FORWARDED');
+        } else {
+            $ip = $_SERVER['REMOTE_ADDR'];
+        }
+        return $ip;
+    }
+
+    public static function getUserAgent(): string
+    {
+        return $_SERVER['HTTP_USER_AGENT'];
+    }
+}
